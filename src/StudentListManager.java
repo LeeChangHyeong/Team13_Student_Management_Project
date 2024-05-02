1de1ff89c005b13f4d9310b403f63fddb2b41c25
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-02 20:14:33 +0900

    ✨update - 특정 수강생을 조회하고 이름과 상태를 수정할 수 있는 메서드 구현

--- src/StudentListManager.java
+++ src/StudentListManager.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class StudentListManager {
@@ -15,13 +18,43 @@ public class StudentListManager {
         System.out.println("수강생 목록을 조회합니다.");
         System.out.println("-----------------------------------------");
         for (Student s : studentsList) {
-
-
             System.out.println("수강생 고유번호: " + s.getNumber());
             System.out.println("수강생 이름: " + s.getName());
             System.out.println("수강생 상태: " + s.getStatus());
             System.out.println(s.getName() + "님이 선택한 과목명: " + s.getSubjectLists());
             System.out.println();
+        }
+    }
+
+    // 번호와 맞는 수강생을 찾아 수정하는 메서드
+    public void modifyStudent(int studentNumber) throws IOException {
+        Student student = null;
+        String lastName = null;
+        Status lastStatus = null;
+
+        for (Student s : studentsList) {
+            if (s.getNumber() == studentNumber) {
+                student = s;
+                lastName = s.getName();
+                lastStatus = s.getStatus();
+            }
+        }
+        if (student == null) {
+            System.out.println("번호와 맞는 수강생이 없습니다.");
+        } else {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("수정할 이름을 입력하세요: ");
+            student.setName(br.readLine());
+            System.out.println("이름이 " + student.getName() + "으로 변경되었습니다.");
+            System.out.print("수정할 상태를 입력하세요 (Green, Red, Yellow 중 하나를 입력하세요.): ");
+            student.setStatus(br.readLine());
+            System.out.println();
+            System.out.println(student.getName() + "의 상태가 " + student.getName() + "(으)로 변경되었습니다.");
+            System.out.println();
+            System.out.println("변경된 정보");
+
+            System.out.println("이름: " + lastName + " " + "->" + " " + student.getName());
+            System.out.println("상태: " + lastStatus + " " + "->" + " " + student.getStatus());
         }
     }
 
