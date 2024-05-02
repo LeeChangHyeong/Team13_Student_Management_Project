7e2282515e8f91de72eb2d6e8bf7c5cb9838e135
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-02 17:17:32 +0900

    ✨update - Student 클래스에 getter 생성, 주석 추가

--- src/Student.java
+++ src/Student.java
@@ -6,10 +6,27 @@ public class Student {
     private Status status;
     private ArrayList<String[]> subjectLists;
 
+    // Student 객체가 생성될때 학생 정보 초기화
     public Student(int number, String name, Status status, ArrayList<String[]> subjectLists) {
         this.number = number;
         this.name = name;
         this.status = status;
         this.subjectLists = subjectLists;
+    }
+    // number getter
+    public int getNumber() {
+        return this.number;
+    }
+    // name getter
+    public String getName() {
+        return this.name;
+    }
+    // status getter
+    public Status getStatus() {
+        return this.status;
+    }
+    // list getter
+    public ArrayList<String[]> getSubjectLists() {
+        return this.subjectLists;
     }
 }
