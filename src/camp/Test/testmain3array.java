a4048f9c6c674960d80ba807d2a18891408b5acd
Imnotcoderdude <topsolo7@naver.com>
2024-05-03 17:02:42 +0900

    testpush

--- src/camp/Test/testmain3array.java
+++ src/camp/Test/testmain3array.java
@@ -3,7 +3,7 @@ package camp.Test;
 import java.util.Scanner;
 // 배열 여러개를 만들어서 각각 요소들을 배치한 로직.
 // 같은 자리의 인덱스끼리는 모두 같은 학생의 정보를 가지고 있다.
-public class Main {
+public class testmain3array {
     public static void main(String[] args) {
         TestClass tClass = new TestClass();
         Scanner sc = new Scanner(System.in);
