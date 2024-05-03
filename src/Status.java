6439f89ff9f92baa6b2d1cce444070839ead4e87
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-03 10:52:39 +0900

    ✨update - String값을 받아와 enum값으로 변경해주고 예외처리 해주는 함수 구현

--- src/Status.java
+++ src/Status.java
@@ -1,5 +1,28 @@
+import Exceptions.BadStatusException;
+
 public enum Status {
-    Green,
-    Red,
-    Yellow;
+    Green("Green"),
+    Red("Red"),
+    Yellow("Yellow");
+
+    private final String status;
+
+    Status(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    // String값을 받아서 상태enum에 맞는 케이스를 찾아주고 찾지 못하면 예외처리를 해주는 함수
+    public static Status getStatusByString(String status) throws BadStatusException {
+        for (Status s : Status.values()) {
+            if (s.getStatus().equals(status)) {
+                return s;
+            }
+        }
+        // 맞지 않는 상태 입력시 예외처리
+        throw new BadStatusException();
+    }
 }
