4369c122cbb55dac596c0c19f5361ab63e5562ec
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-07 11:30:16 +0900

    🎉 add - 과목들을 저장하는 Subjects 클래스 생성

---
+++ src/Subjects.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Subjects {
+    // 필수 과목 리스트
+    private ArrayList<String> mainSubjectList;
+
+    // 선택 과목 리스트
+    private ArrayList<String> choiceSubjcetList;
+
+    public void setMainSubjects() {
+
+    }
+
+    public void getMainSubjects() {
+
+    }
+
+    public void setChoiceSubjcetList() {
+
+    }
+
+    public void getChoiceSubjcetList() {
+
+    }
+}
