1f17bd7ac0fa1d1a0f07322ee77cf4aeed6b831f
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-07 11:38:08 +0900

    ✨ update - 필수, 선택 과목 getter, setter 구현

--- src/Subjects.java
+++ src/Subjects.java
@@ -7,19 +7,33 @@ public class Subjects {
     // 선택 과목 리스트
     private ArrayList<String> choiceSubjcetList;
 
-    public void setMainSubjects() {
-
+    public void setMainSubjects(ArrayList<String> mainSubjectList) {
+        this.mainSubjectList = mainSubjectList;
     }
 
     public void getMainSubjects() {
-
+        System.out.println("필수 과목 조회: " + mainSubjectList);
     }
 
-    public void setChoiceSubjcetList() {
-
+    public void setChoiceSubjcetList(ArrayList<String> mainSubjectList) {
+        this.mainSubjectList = mainSubjectList;
     }
 
     public void getChoiceSubjcetList() {
+        System.out.println("선택 과목 조회: " + choiceSubjcetList);
+    }
 
+    // 전체 출력
+    public void getAllSubjectList() {
+        ArrayList<String> allSubjectList = new ArrayList<>();
+        for(String s: mainSubjectList) {
+            allSubjectList.add(s);
+        }
+
+        for(String s: choiceSubjcetList) {
+            allSubjectList.add(s);
+        }
+
+        System.out.println("전체 과목 조회: " + allSubjectList);
     }
 }
