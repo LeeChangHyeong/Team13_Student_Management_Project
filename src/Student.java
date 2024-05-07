901f28db88ddc2440e4b08614f82fca6a6cf6c75
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-07 10:53:20 +0900

    🩹fix - Student 클래스 생성자 삭제

--- src/Student.java
+++ src/Student.java
@@ -8,14 +8,6 @@ public class Student {
     private Status status;
     private ArrayList<String> subjectLists;
 
-    // Student 객체가 생성될때 학생 정보 초기화
-    public Student(int number, String name, Status status, ArrayList<String> subjectLists) {
-        this.number = number;
-        this.name = name;
-        this.status = status;
-        this.subjectLists = subjectLists;
-    }
-
     // number getter
     public int getNumber() {
         return this.number;
