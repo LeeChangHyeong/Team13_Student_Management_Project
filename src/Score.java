88cdbc34d131de865e32d2dce5920f7d483faf23
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-05-07 20:20:15 +0900

    🩹 fix - Score 클래스에 id -> 수강생 고유번호 추가

--- src/Score.java
+++ src/Score.java
@@ -2,13 +2,15 @@ import java.util.*;
 
 // 점수 등록을 위한 ScoreEntry 클래스 정의
 class Score {
+    private int id; // 수강생 고유 번호
     private String subject; // 과목명
     private int round;      // 회차
     private int score;      // 점수
     private String grade;   // 등급
 
     // 생성자: 과목명, 회차, 점수를 받아서 객체를 초기화함
-    public Score(String subject, int round, int score) {
+    public Score(int id, String subject, int round, int score) {
+        this.id = id;
         this.subject = subject;
         this.round = round;
         this.score = score;
