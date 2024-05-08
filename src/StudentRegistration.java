126d378bc905fc33ca3b067fc98e5ec3bd4f852a
Imnotcoderdude <topsolo7@naver.com>
2024-05-08 19:55:53 +0900

    fix : 수강생 상태 조회 오입력 수정

--- src/StudentRegistration.java
+++ src/StudentRegistration.java
@@ -7,11 +7,54 @@ import java.io.*;
 public class StudentRegistration {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    private ArrayList<Student> studentArrayList = new ArrayList<>();
+    public static ArrayList<Student> studentArrayList = new ArrayList<>();
     private StudentListManager studentListManager = new StudentListManager(studentArrayList);
 
     // 메인 클래스 내에서 객체화 시켜 사용하면 되는 메소드
     public void start() throws BadStatusException, IOException {
+        // 초기 데이터 등록
+        Student lch = new Student();
+        Student karina = new Student();
+
+        ArrayList<String> lchMainSubjectList = new ArrayList<>();
+        ArrayList<String> lchChoiceSubjectList = new ArrayList<>();
+        Subjects lchSubjects = new Subjects();
+
+        ArrayList<String> karinaMainSubjectList = new ArrayList<>();
+        ArrayList<String> karinaChoiceSubjectList = new ArrayList<>();
+        Subjects karinaSubjects = new Subjects();
+
+        lchMainSubjectList.add("Java");
+        lchMainSubjectList.add("객체지향");
+        lchMainSubjectList.add("JPA");
+        lchChoiceSubjectList.add("디자인 패턴");
+        lchChoiceSubjectList.add("MongoDB");
+        lchSubjects.setMainSubjects(lchMainSubjectList);
+        lchSubjects.setChoiceSubjcetList(lchChoiceSubjectList);
+
+        karinaMainSubjectList.add("Java");
+        karinaMainSubjectList.add("객체지향");
+        karinaMainSubjectList.add("Mysql");
+        karinaChoiceSubjectList.add("Redis");
+        karinaChoiceSubjectList.add("MongoDB");
+        karinaSubjects.setMainSubjects(karinaMainSubjectList);
+        karinaSubjects.setChoiceSubjcetList(karinaChoiceSubjectList);
+
+        lch.setSubjects(lchSubjects);
+        lch.setStatus("Red");
+        lch.setId(1);
+        lch.setName("이창형");
+
+        karina.setSubjects(karinaSubjects);
+        karina.setStatus("Green");
+        karina.setId(2);
+        karina.setName("카리나");
+
+
+        studentArrayList.add(lch);
+        studentArrayList.add(karina);
+
+
         while (true) {
             System.out.println("-----------------------------------------");
             System.out.println("수강생 등록 (1)");
@@ -19,7 +62,8 @@ public class StudentRegistration {
             System.out.println("정보 보기 (3)");
             System.out.println("정보 수정 (4)");
             System.out.println("정보 삭제 (5)");
-            System.out.println("프로그램 종료 (6)");
+            System.out.println("평균 등급 조회 (6)");
+            System.out.println("프로그램 종료 (7)");
             System.out.print("입력 : ");
             String choice = br.readLine();
             System.out.println("-----------------------------------------");
@@ -34,33 +78,36 @@ public class StudentRegistration {
 
                 studentArrayList.add(student);
             } else if (choice.equals("2")) {
-                // TODO: 성적 등록 구현
-//                System.out.print("성적을 등록할 수강생의 고유 번호 입력: ");
-//                Score score = new Score(Integer.parseInt(br.readLine()));
                 ScoreRegistration scoreRegistration = new ScoreRegistration();
                 scoreRegistration.setScore();
-
-
             } else if (choice.equals("3")) {
                 while (true) {
+                    System.out.println("-----------------------------------------");
                     System.out.println("전체 수강생 조회 (1)");
                     System.out.println("상태별 수강생 조회 (2)");
                     System.out.println("돌아가기 (3)");
                     System.out.print("입력 : ");
                     String str = br.readLine();
-                    System.out.println("-----------------------------------------");
                     if (str.equals("1")) {
                         studentListManager.printStudentList();
                     } else if (str.equals("2")) {
-                        System.out.print("조회하고 싶은 수강생 상태를 입력 (Green, Yellow, Red 중 하나를 입력하세요.): ");
-                        studentListManager.printStudentListByStatus(br.readLine());
+                        while (true) {
+                            System.out.print("조회하고 싶은 수강생 상태를 입력 (Green, Yellow, Red 중 하나를 입력하세요.): ");
+                            String status = br.readLine();
+                            if (status.equals("Green") || status.equals("Yellow") || status.equals("Red")) {
+                                studentListManager.printStudentListByStatus(status);
+                                break;
+                            } else {
+                                System.out.println("!!!!정확한 상태명을 입력하세요!!!!");
+                            }
+                        }
+
                     } else if (str.equals("3")) {
                         break;
                     } else {
                         System.out.println("올바른 숫자를 입력해주세요.");
                     }
                 }
-            // 학생 고유번호에 int 타입이 아닌 값 입력시 재입력 요구 설정
             } else if (choice.equals("4")) {
                 while (true) {
                     System.out.println("수정하고 싶은 수강생 고유번호 입력: ");
@@ -73,11 +120,26 @@ public class StudentRegistration {
                         System.out.println("숫자만 입력하세요!");
                     }
                 }
-
             } else if (choice.equals("5")) {
                 System.out.println("삭제하고 싶은 수강생 고유번호 입력: ");
                 studentListManager.deleteStudentData(Integer.parseInt(br.readLine()));
             } else if (choice.equals("6")) {
+                while (true) {
+                    System.out.println("모든 수강생의 과목별 평균 등급 조회 (1)");
+                    System.out.println("상태별 수강생의 필수 과목 평균 등급 조회 (2)");
+                    String str = br.readLine();
+                    if (str.equals("1")) {
+                        ScoreManager.inquiryAverageGradeBySubject();
+                        break;
+                    } else if (str.equals("2")) {
+                        System.out.print("조회하고 싶은 수강생들의 상태를 입력하세요 (Green, Yellow, Red 중 입력하세요.): ");
+                        ScoreManager.inquiryAverageGradeByStatus(br.readLine());
+                        break;
+                    } else {
+                        System.out.println("올바른 숫자를 입력해주세요.");
+                    }
+                }
+            } else if (choice.equals("7")) {
                 return;
             } else {
                 System.out.println("정확한 번호를 입력하세요");
@@ -100,7 +162,7 @@ public class StudentRegistration {
                 }
             }
             // 조규성 : 학생 이름에 숫자 + 문자가 올 경우 등록이 되는 버그 발생
-            // 조규성 : if (Character.isDigit(c)) 으로 변경.
+            // 조규성 : 위에 if (Character.hasDigit(c)) 을 if (Character.isDigit(c)) 으로 변경.
             if (isString) {
                 System.out.println("학생이름에는 숫자가 입력될 수 없습니다.");
             } else {
@@ -121,6 +183,7 @@ public class StudentRegistration {
 
         return mainsubjectnameok.contains(subject);
     }
+
     // 선택 과목명 검사 메소드
     private boolean choiceSubjectOk(String subject) {
         Set<String> choicesubjectnameok = new HashSet<>();
@@ -144,7 +207,7 @@ public class StudentRegistration {
             String subject = br.readLine();
 
             if (subject.equals("exit")) {
-                if (mainSubjectList.size() < 3)  {
+                if (mainSubjectList.size() < 3) {
                     System.out.println("3개 이상 입력해야 합니다.");
                     continue;
                 } else {
@@ -169,7 +232,7 @@ public class StudentRegistration {
             String subject = br.readLine();
 
             if (subject.equals("exit")) {
-                if (choiceSubjectList.size() < 2)  {
+                if (choiceSubjectList.size() < 2) {
                     System.out.println("2개 이상 입력해야 합니다.");
                     continue;
                 } else {
